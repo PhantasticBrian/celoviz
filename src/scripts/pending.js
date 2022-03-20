4f2582aa81e90c2ec57cb144a521da2815e3a80e
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-03-19 17:21:21 -0700

    Fix uncommited changes

--- src/scripts/pending.js
+++ src/scripts/pending.js
@@ -1,27 +1,27 @@
 var settings = require("../constants/settings.json");
 var ethers = require("ethers");
 
-var init = function () {
-  var customWsProvider = new ethers.providers.WebSocketProvider(
+const init = () => {
+  var wsProvider = new ethers.providers.WebSocketProvider(
     settings.wss_provider
   );
 
-  customWsProvider.on("pending", (tx) => {
-    customWsProvider.getTransaction(tx).then(function (transaction) {
+  wsProvider.on("pending", (tx) => {
+    wsProvider.getTransaction(tx).then((transaction) => {
       console.log(transaction);
     });
   });
 
-  customWsProvider._websocket.on("error", async () => {
+  wsProvider._websocket.on("error", async () => {
     console.log(`Unable to connect to ${ep.subdomain} retrying in 3s...`);
     setTimeout(init, 3000);
   });
 
-  customWsProvider._websocket.on("close", async (code) => {
+  wsProvider._websocket.on("close", async (code) => {
     console.log(
       `Connection lost with code ${code}! Attempting reconnect in 3s...`
     );
-    customWsProvider._websocket.terminate();
+    wsProvider._websocket.terminate();
     setTimeout(init, 3000);
   });
 };
